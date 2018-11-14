37e8a94838e442b9d3d490f4dfc914c9a8c36096
侯珏 <wb-hj291239@alibaba-inc.com>
2018-11-14 16:14:33 +0800

    firstDemo

---
+++ src/main/java/com/concurrent/demo/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.concurrent.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by houjue on 2018/11/13.
+ */
+@Controller
+public class TestController {
+}
